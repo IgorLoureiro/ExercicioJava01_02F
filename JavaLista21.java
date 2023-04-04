b499d2951c387572a89291938c25517dd1faa677
IgorLoureiro <igor.loureiro8@hortmail.com>
2023-04-03 21:00:10 -0300

    ExercicioJava2_Lista2

---
+++ JavaLista21.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class JavaLista21 {
+    public static void main(String[] args) throws Exception{
+
+        Double numero;
+
+        System.out.println("Informe um número");
+        Scanner teclado = new Scanner(System.in);
+        numero = teclado.nextDouble();
+
+        System.out.println("O numero informado foi: " + numero);
+
+    }
+}
